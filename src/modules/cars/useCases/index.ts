b2e624fb74eaf0c82bdfa2d28e08d1bb018a3d8c
mpgxc <mpgx5.c@gmail.com>
2021-04-18 13:05:41 -0300

    feat: usecase default export

---
+++ src/modules/cars/useCases/index.ts
@@ -0,0 +1,4 @@
+export { createCategoryContoller } from './CreateCategory';
+export { createSpecificationController } from './CreateSpecification';
+export { listCategoriesController } from './ListCategories';
+export { listSpecificationController } from './ListSpecifications';
