1269a10c7fb01ad717af6614bb59ac628159ba9c
mpgxc <mpgx5.c@gmail.com>
2021-04-29 01:09:56 -0300

    doc: Add docs using swagger provider

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 
 import routes from './routes';
+import ApiSchema from './shared/docs/swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
 app.use(routes);
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(ApiSchema));
 
 export { app };
