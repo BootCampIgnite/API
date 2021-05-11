f5126a33ee4755941b324d3c35653632966042f2
mpgxc <mpgx5.c@gmail.com>
2021-05-11 07:12:00 -0300

    feat: Added abstract baseEntity and User entity extending from baseEntity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,26 @@
+import { Column, Entity } from 'typeorm';
+
+import { BaseEntity } from '../../../core/BaseEntity';
+
+@Entity('users')
+class User extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  driver_license: string;
+
+  @Column({ unique: true })
+  is_admin: string;
+}
+
+export { User };
