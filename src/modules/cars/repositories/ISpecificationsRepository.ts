0ae49d5a92b07d447e9c181be6fca48017700ff1
mpgxc <mpgx5.c@gmail.com>
2021-05-11 08:34:36 -0300

    refactor: Removed DTO to especific folder

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,9 +1,5 @@
+import { ICreateSpecificationsDTO } from '../dtos/ICreateSpecificationsDTO';
 import { Specification } from '../entities/Specification';
-
-interface ICreateSpecificationsDTO {
-  name: string;
-  description: string;
-}
 
 interface ISpecificationsRepository {
   create({ description, name }: ICreateSpecificationsDTO): Promise<void>;
