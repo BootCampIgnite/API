6bd8ef7bac55e82fca676231493ee843a36c0334
mpgxc <mpgx5.c@gmail.com>
2021-05-12 06:57:46 -0300

    feat: errors handling class and global catch middleware

---
+++ src/infra/http/middlewares/ErrorHandler.ts
@@ -0,0 +1,26 @@
+import { Response, Request, NextFunction } from 'express';
+
+import { AppException } from '../../../shared/errors/AppException';
+
+class ErrorHandler {
+  static handle(
+    err: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ): Response {
+    if (err instanceof AppException) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal Server Error - ${err.message}`,
+    });
+  }
+}
+
+export { ErrorHandler };
