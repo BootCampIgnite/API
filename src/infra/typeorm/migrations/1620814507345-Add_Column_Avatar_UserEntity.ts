b28e617dbfd6ce28a883a8cb4dfd30c4d7a85a74
mpgxc <mpgx5.c@gmail.com>
2021-05-12 13:13:40 -0300

    feat: added column 'avatar' to user entity

---
+++ src/infra/typeorm/migrations/1620814507345-Add_Column_Avatar_UserEntity.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddColumnAvatarUserEntity1620814507345
+  implements MigrationInterface {
+  name = 'AddColumnAvatarUserEntity1620814507345';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "avatar" character varying`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "avatar"`);
+  }
+}
