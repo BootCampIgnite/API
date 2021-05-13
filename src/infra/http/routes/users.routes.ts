1c389e41a2105d00268fd1eecbf96ee2f35a0dae
mpgxc <mpgx5.c@gmail.com>
2021-05-13 06:07:57 -0300

    feat: call upload function to upload avatar

--- src/infra/http/routes/users.routes.ts
+++ src/infra/http/routes/users.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../../../configs/upload';
 import { RegisterUserController } from '../../../modules/accounts/useCases/RegisterUser/RegisterUserController';
+import { UpdateUserAvatarController } from '../../../modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController';
+import { EnsureAuthenticate } from '../middlewares/EnsureAuthenticate';
 
 const usersRouter = Router();
+const uploadAvatar = multer(uploadConfig.upload('./uploads/avatar'));
 
 const registerUserController = new RegisterUserController();
 usersRouter.post('/', registerUserController.handle);
+
+usersRouter.use(EnsureAuthenticate.handle);
+
+const updateUserController = new UpdateUserAvatarController();
+usersRouter.patch(
+  '/avatar',
+  uploadAvatar.single('avatar'),
+  updateUserController.handle,
+);
 
 export { usersRouter };
