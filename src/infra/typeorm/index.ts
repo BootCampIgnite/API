ce848666baf0c08f15735c2a87f18650ab21bfec
mpgxc <mpgx5.c@gmail.com>
2021-08-28 18:17:38 -0300

    feat: added seed admin account

--- src/infra/typeorm/index.ts
+++ src/infra/typeorm/index.ts
@@ -1,7 +1,13 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 
 getConnectionOptions().then(options => {
   createConnection({
     ...options,
-  });
+  })
+    .then((connection: Connection) => {
+      console.log(`🟩 Database ${connection.name} Connection Successful!`);
+    })
+    .catch((error: Error) => {
+      console.log(`🟥 Database ${error.message} Connection failed!`);
+    });
 });
---
+++ src/infra/typeorm/seeds/CreateAdminUser.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+
+import { hash } from 'bcrypt';
+import { getRepository } from 'typeorm';
+import { Seeder } from 'typeorm-seeding';
+
+import { User } from '@modules/accounts/entities/User';
+
+class CreateAccountAdmin implements Seeder {
+  public async run(): Promise<void> {
+    const usersRepository = getRepository(User);
+
+    const passwordHashed = await hash('admin', 8);
+
+    const user = Object.assign(new User(), {
+      is_admin: true,
+      name: 'Administrador',
+      username: 'admin',
+      password: passwordHashed,
+      email: 'admin@agilitytelecom.com',
+      driver_license: '123456789',
+    });
+
+    await usersRepository.save(user);
+  }
+}
+
+export default CreateAccountAdmin;
