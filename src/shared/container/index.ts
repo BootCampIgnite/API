934941b20d3c56b7904a293d92877ec5b8617f54
mpgxc <mpgx5.c@gmail.com>
2021-08-28 07:07:37 -0300

    feat: added entry point on container to access cars repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from 'tsyringe';
 
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { UsersRepository } from '@modules/accounts/repositories/typeorm/UsersRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { CarsRepository } from '@modules/cars/repositories/typeorm/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/repositories/typeorm/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/repositories/typeorm/SpecificationsRepository';
 
@@ -21,3 +23,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
